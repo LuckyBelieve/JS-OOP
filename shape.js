b42c9d19583ae4696907de612aa6407862e5da46
Lucky <117139436+LuckyBelieve@users.noreply.github.com>
2023-01-09 17:02:37 +0200

    making instant objects using constructor functions

---
+++ shape.js
@@ -0,0 +1,15 @@
+function Shape(){
+    this.name ;
+    this.width;
+    this.height;
+    this.calculateArea = function(){
+        console.log("the area of "+this.name+" is "+this.width*this.height);
+    }
+
+}
+const newArea = new Shape()
+newArea.name = 'Rectangle';
+newArea.width = 10;
+newArea.height = 5;
+
+newArea.calculateArea();
